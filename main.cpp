02875ced6828da9e26343ed98ed78708a82f2389
Zachary Rudin <ZacARmailbox@proton.me>
2025-06-06 22:55:43 -0700

    Fixed the main.cpp file its now more in line with the indented structure chart

--- main.cpp
+++ main.cpp
@@ -10,24 +10,22 @@
 
 using namespace std;
 
-void buildTreeAndHash(const string& filename, BinaryTree<Creature>& Ctree);
-void displayManager(const BinaryTree<Creature>& Ctree);
-void searchManager(const BinaryTree<Creature>& Ctree);
-void deleteManager(BinaryTree<Creature>& Ctree);
-void updateManager(BinaryTree<Creature>& Ctree);
+void printWelcome();
+void buildCreatureList();
+void runMainMenu();
+void printGoodbye();
 
 int main() {
     
     string filename;
     BinaryTree<Creature> Ctree;
 
-    cout << "Enter the name of the file containing the creature data: ";
-    getline(cin, filename);
-    buildTreeAndHash(filename, Ctree); // maybe use a different name or two functions
-
-    displayManager(Ctree); // these functions will do a lot feel free to change it as needed
-    searchManager(Ctree);
-    deleteManager(Ctree);
-    updateManager(Ctree);
+    printWelcome();
+    buildCreatureList();
+    runMainMenu();
+    printGoodbye();
+    // cout << "Enter the name of the file containing the creature data: ";
+    // getline(cin, filename);
+  
 
 }
